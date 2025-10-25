93e6aaf7fd1973f0485c8ef7cdeada25c3c30c2a
LaWin0116 <chb-0116@naver.com>
2025-10-25 14:41:20 +0900

    add power supply

--- src/main/java/io/github/lawin/buildpc/core/item/ModCreativeModeTabs.java
+++ src/main/java/io/github/lawin/buildpc/core/item/ModCreativeModeTabs.java
@@ -25,6 +25,7 @@ public class ModCreativeModeTabs {
                 output.accept(ModItems.RAM);
                 output.accept(ModItems.MD2_SSD);
                 output.accept(ModItems.MOTHER_BOARD);
+                output.accept(ModItems.POWER_SUPPLY);
             }).build());
     // TODO: PC case 블록 추가 후에 아이콘 변경, line: 31 아이템 등록부 수정
     public static final Supplier<CreativeModeTab> MISC_HARDWARE = CREATIVE_MODE_TAB.register("misc_hardware_tab", () -> CreativeModeTab.builder()
--- src/main/java/io/github/lawin/buildpc/core/item/ModItems.java
+++ src/main/java/io/github/lawin/buildpc/core/item/ModItems.java
@@ -17,6 +17,7 @@ public class ModItems {
     public static final DeferredItem<Item> RAM = ITEMS.register("ram", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> MD2_SSD = ITEMS.register("md2_ssd", () -> new  Item(new Item.Properties()));
     public static final DeferredItem<Item> MOTHER_BOARD = ITEMS.register("mother_board", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> POWER_SUPPLY = ITEMS.register("power_supply", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
