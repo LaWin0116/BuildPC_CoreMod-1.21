6dfd1a2a9114b73de3de055c5026e2eed8496556
LaWin0116 <chb-0116@naver.com>
2025-12-18 23:11:37 +0900

    make pc cases datas by datagen

--- src/main/java/io/github/lawin/buildpc/core/block/ModBlocks.java
+++ src/main/java/io/github/lawin/buildpc/core/block/ModBlocks.java
@@ -2,8 +2,10 @@ package io.github.lawin.buildpc.core.block;
 
 import io.github.lawin.buildpc.core.BuildPCCore;
 
+import io.github.lawin.buildpc.core.block.custom.PcCaseBlock;
 import io.github.lawin.buildpc.core.item.ModItems;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
@@ -13,6 +15,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.EnumMap;
 import java.util.function.Supplier;
 
 public class ModBlocks {
@@ -25,25 +28,35 @@ public class ModBlocks {
                     .sound(SoundType.STONE)));
 
     /** pc case 블록의 공통 속성 */
-    private static final BlockBehaviour.Properties PC_CASES = BlockBehaviour.Properties.of().strength(1f).noOcclusion().sound(SoundType.METAL);
+    private static final BlockBehaviour.Properties PC_CASE_PROPERTIES = BlockBehaviour.Properties.of().strength(1f).noOcclusion().sound(SoundType.METAL);
+
+    public static final EnumMap<DyeColor, DeferredBlock<Block>> PC_CASES = new EnumMap<>(DyeColor.class);
+    static {
+        for (DyeColor color : DyeColor.values()) {
+            String name = color.getName() + "_pc_case";
+            DeferredBlock<Block> block = BLOCKS.register(name, () -> new PcCaseBlock(PC_CASE_PROPERTIES));
+            PC_CASES.put(color, block);
+            registerBlockItem(name, block);
+        }
+    }
 
     // public static final DeferredBlock<Block> PC_CASE = registerBlock("pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> WHITE_PC_CASE = registerBlock("white_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> LIGHT_GRAY_PC_CASE = registerBlock("light_gray_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> GRAY_PC_CASE = registerBlock("gray_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> BLACK_PC_CASE = registerBlock("black_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> BROWN_PC_CASE = registerBlock("brown_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> RED_PC_CASE = registerBlock("red_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> ORANGE_PC_CASE = registerBlock("orange_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> YELLOW_PC_CASE = registerBlock("yellow_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> LIME_PC_CASE = registerBlock("lime_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> GREEN_PC_CASE = registerBlock("green_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> CYAN_PC_CASE = registerBlock("cyan_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> LIGHT_BLUE_PC_CASE = registerBlock("light_blue_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> BLUE_PC_CASE = registerBlock("blue_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> PURPLE_PC_CASE = registerBlock("purple_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> MAGENTA_PC_CASE = registerBlock("magenta_pc_case", () -> new Block(PC_CASES));
-    public static final DeferredBlock<Block> PINK_PC_CASE = registerBlock("pink_pc_case", () -> new Block(PC_CASES));
+    public static final DeferredBlock<Block> WHITE_PC_CASE = PC_CASES.get(DyeColor.WHITE);
+    public static final DeferredBlock<Block> LIGHT_GRAY_PC_CASE = PC_CASES.get(DyeColor.LIGHT_GRAY);
+    public static final DeferredBlock<Block> GRAY_PC_CASE = PC_CASES.get(DyeColor.GRAY);
+    public static final DeferredBlock<Block> BLACK_PC_CASE = PC_CASES.get(DyeColor.BLACK);
+    public static final DeferredBlock<Block> BROWN_PC_CASE = PC_CASES.get(DyeColor.BROWN);
+    public static final DeferredBlock<Block> RED_PC_CASE = PC_CASES.get(DyeColor.RED);
+    public static final DeferredBlock<Block> ORANGE_PC_CASE = PC_CASES.get(DyeColor.ORANGE);
+    public static final DeferredBlock<Block> YELLOW_PC_CASE = PC_CASES.get(DyeColor.YELLOW);
+    public static final DeferredBlock<Block> LIME_PC_CASE = PC_CASES.get(DyeColor.LIME);
+    public static final DeferredBlock<Block> GREEN_PC_CASE = PC_CASES.get(DyeColor.GREEN);
+    public static final DeferredBlock<Block> CYAN_PC_CASE = PC_CASES.get(DyeColor.CYAN);
+    public static final DeferredBlock<Block> LIGHT_BLUE_PC_CASE = PC_CASES.get(DyeColor.LIGHT_BLUE);
+    public static final DeferredBlock<Block> BLUE_PC_CASE = PC_CASES.get(DyeColor.BLUE);
+    public static final DeferredBlock<Block> PURPLE_PC_CASE = PC_CASES.get(DyeColor.PURPLE);
+    public static final DeferredBlock<Block> MAGENTA_PC_CASE = PC_CASES.get(DyeColor.MAGENTA);
+    public static final DeferredBlock<Block> PINK_PC_CASE = PC_CASES.get(DyeColor.PINK);
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/io/github/lawin/buildpc/core/datagen/ModBlockStateProvider.java
+++ src/main/java/io/github/lawin/buildpc/core/datagen/ModBlockStateProvider.java
@@ -3,7 +3,9 @@ package io.github.lawin.buildpc.core.datagen;
 import io.github.lawin.buildpc.core.BuildPCCore;
 import io.github.lawin.buildpc.core.block.ModBlocks;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.DyeColor;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.registries.DeferredBlock;
 
@@ -15,9 +17,36 @@ public class ModBlockStateProvider extends BlockStateProvider {
     @Override
     protected void registerStatesAndModels() {
         blockWithItem(ModBlocks.TEST_BLOCK);
+
+        for (DyeColor color : DyeColor.values()) {
+            horizontalBlockWithItem(ModBlocks.PC_CASES.get(color));
+        }
     }
 
     private void blockWithItem(DeferredBlock<?> deferredBlock) {
         simpleBlockWithItem(deferredBlock.get(), cubeAll(deferredBlock.get()));
+    }
+
+    private void horizontalBlockWithItem(DeferredBlock<?> deferredBlock) {
+//        horizontalBlock(deferredBlock.get(), blockState -> models().getExistingFile(modLoc("block/" + deferredBlock.getId().getPath())));
+//        blockItem(deferredBlock);
+        String name = deferredBlock.getId().getPath();
+        ModelFile model = models()
+                .withExistingParent(
+                        name,
+                        modLoc("block/pc_case") // 부모 모델
+                )
+                .texture("0", modLoc("block/" + name))
+                .texture("particle", modLoc("block/" + name));
+        horizontalBlock(deferredBlock.get(), model);
+        simpleBlockItem(deferredBlock.get(), model);
+    }
+
+    private void blockItem(DeferredBlock<?> deferredBlock) {
+        simpleBlockItem(deferredBlock.get(), new ModelFile.UncheckedModelFile("buildpccoremod:block/" + deferredBlock.getId().getPath()));
+    }
+
+    private void blockItem(DeferredBlock<?> deferredBlock, String appendix) {
+        simpleBlockItem(deferredBlock.get(), new ModelFile.UncheckedModelFile("buildpccoremod:block/" + deferredBlock.getId().getPath() + appendix));
     }
 }
