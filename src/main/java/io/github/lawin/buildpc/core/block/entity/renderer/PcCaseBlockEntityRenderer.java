f5b3208e2176dde52d3cdbae22728d2b3f2c52e7
LaWin <lawin0116@gmail.com>
2026-02-14 22:41:15 +0900

    Renderer: CPU

--- src/main/java/io/github/lawin/buildpc/core/block/entity/renderer/PcCaseBlockEntityRenderer.java
+++ src/main/java/io/github/lawin/buildpc/core/block/entity/renderer/PcCaseBlockEntityRenderer.java
@@ -28,19 +28,18 @@ public class PcCaseBlockEntityRenderer implements BlockEntityRenderer<PcCaseBloc
 	    if (level == null) return;
 
 	    pPoseStack.pushPose();
-	    pPoseStack.translate(0.5D, 0.5D, 0.5D);
+	    pPoseStack.translate(.5D, .5D, .5D);
 
 		// motherboard (slots 0)
-	    // Motherboard (slot 0)
 	    ItemStack motherboard = pBlockEntity.inventory.getStackInSlot(0);
 	    if (!motherboard.isEmpty()) {
 
 		    pPoseStack.pushPose();
 
-		    pPoseStack.translate(-0.1D, 0.17D, -0.18D);
+		    pPoseStack.translate(-.1D, .17D, -.18D);
 			pPoseStack.mulPose(Axis.XP.rotationDegrees(180));
 			pPoseStack.mulPose(Axis.ZP.rotationDegrees(180));
-		    pPoseStack.scale(0.45F, 0.45F, 0.45F);
+		    pPoseStack.scale(.45F, .45F, .45F);
 
 		    itemRenderer.renderStatic(
 				    motherboard,
@@ -57,32 +56,52 @@ public class PcCaseBlockEntityRenderer implements BlockEntityRenderer<PcCaseBloc
 	    }
 
 
-	    // GPU (PCIe) (slots 9-10)
-	    ItemStack gpu = pBlockEntity.inventory.getStackInSlot(9);
-	    if (!gpu.isEmpty()) {
-		    pPoseStack.pushPose();
-		    pPoseStack.translate(0.0D, 0.15D, -0.1D);
-		    pPoseStack.scale(0.8F, 0.8F, 0.8F);
-
-		    itemRenderer.renderStatic(
-				    gpu,
-				    ItemDisplayContext.FIXED,
-				    pPackedLight,
-				    pPackedOverlay,
-				    pPoseStack,
-				    pBufferSource,
-				    level,
-				    0
-		    );
-		    pPoseStack.popPose();
-	    }
+	    // PCIe (slots 9-10)
+//	    ItemStack PCIeI = pBlockEntity.inventory.getStackInSlot(9);
+//	    if (!PCIeI.isEmpty()) {
+//		    pPoseStack.pushPose();
+//		    pPoseStack.translate(0.0D, 0.15D, -0.1D);
+//		    pPoseStack.scale(0.8F, 0.8F, 0.8F);
+//
+//		    itemRenderer.renderStatic(
+//				    PCIeI,
+//				    ItemDisplayContext.FIXED,
+//				    pPackedLight,
+//				    pPackedOverlay,
+//				    pPoseStack,
+//				    pBufferSource,
+//				    level,
+//				    0
+//		    );
+//		    pPoseStack.popPose();
+//	    }
+//
+//	    ItemStack PCIeII = pBlockEntity.inventory.getStackInSlot(10);
+//	    if (!PCIeII.isEmpty()) {
+//		    pPoseStack.pushPose();
+//		    pPoseStack.translate(0.0D, 0.15D, -0.1D);
+//		    pPoseStack.scale(0.8F, 0.8F, 0.8F);
+//
+//		    itemRenderer.renderStatic(
+//				    PCIeII,
+//				    ItemDisplayContext.FIXED,
+//				    pPackedLight,
+//				    pPackedOverlay,
+//				    pPoseStack,
+//				    pBufferSource,
+//				    level,
+//				    0
+//		    );
+//		    pPoseStack.popPose();
+//	    }
 
 	    // CPU (slots 1)
 	    ItemStack cpu = pBlockEntity.inventory.getStackInSlot(1);
 	    if (!cpu.isEmpty()) {
 		    pPoseStack.pushPose();
-		    pPoseStack.translate(0.0D, 0.25D, 0.0D);
-		    pPoseStack.scale(0.4F, 0.4F, 0.4F);
+		    pPoseStack.translate(-.185D, .255D, -.16D);
+		    pPoseStack.mulPose(Axis.XP.rotationDegrees(180));
+		    pPoseStack.scale(.26F, .26F, .26F);
 
 		    itemRenderer.renderStatic(
 				    cpu,
@@ -98,48 +117,65 @@ public class PcCaseBlockEntityRenderer implements BlockEntityRenderer<PcCaseBloc
 	    }
 
 	    // RAM (slots 3-6)
-	    for (int i = 3; i <= 6; i++) {
-		    ItemStack ram = pBlockEntity.inventory.getStackInSlot(i);
-		    if (ram.isEmpty()) continue;
-
-		    pPoseStack.pushPose();
-		    pPoseStack.translate(-0.15D + (i - 3) * 0.1D, 0.25D, 0.0D);
-		    pPoseStack.scale(0.35F, 0.35F, 0.35F);
-
-		    itemRenderer.renderStatic(
-				    ram,
-				    ItemDisplayContext.FIXED,
-				    pPackedLight,
-				    pPackedOverlay,
-				    pPoseStack,
-				    pBufferSource,
-				    level,
-				    0
-		    );
-		    pPoseStack.popPose();
-	    }
+//	    for (int i = 3; i <= 6; i++) {
+//		    ItemStack ram = pBlockEntity.inventory.getStackInSlot(i);
+//		    if (ram.isEmpty()) continue;
+//
+//		    pPoseStack.pushPose();
+//		    pPoseStack.translate(-0.15D + (i - 3) * 0.1D, 0.25D, 0.0D);
+//		    pPoseStack.scale(0.35F, 0.35F, 0.35F);
+//
+//		    itemRenderer.renderStatic(
+//				    ram,
+//				    ItemDisplayContext.FIXED,
+//				    pPackedLight,
+//				    pPackedOverlay,
+//				    pPoseStack,
+//				    pBufferSource,
+//				    level,
+//				    0
+//		    );
+//		    pPoseStack.popPose();
+//	    }
 
 	    // M.2 (slots 7-8)
-	    for (int i = 7; i <= 8; i++) {
-		    ItemStack ssd = pBlockEntity.inventory.getStackInSlot(i);
-		    if (ssd.isEmpty()) continue;
-
-		    pPoseStack.pushPose();
-		    pPoseStack.translate(0.0D, 0.18D, 0.15D + (i - 7) * 0.08D);
-		    pPoseStack.scale(0.3F, 0.3F, 0.3F);
-
-		    itemRenderer.renderStatic(
-				    ssd,
-				    ItemDisplayContext.FIXED,
-				    pPackedLight,
-				    pPackedOverlay,
-				    pPoseStack,
-				    pBufferSource,
-				    level,
-				    0
-		    );
-		    pPoseStack.popPose();
-	    }
+//	    ItemStack Md2SSDI = pBlockEntity.inventory.getStackInSlot(7);
+//	    if (!Md2SSDI.isEmpty()) {
+//		    pPoseStack.pushPose();
+//		    pPoseStack.translate(0.0D, 0.25D, 0.0D);
+//		    pPoseStack.scale(0.4F, 0.4F, 0.4F);
+//
+//		    itemRenderer.renderStatic(
+//				    Md2SSDI,
+//				    ItemDisplayContext.FIXED,
+//				    pPackedLight,
+//				    pPackedOverlay,
+//				    pPoseStack,
+//				    pBufferSource,
+//				    level,
+//				    0
+//		    );
+//		    pPoseStack.popPose();
+//	    }
+//
+//	    ItemStack Md2SSDII = pBlockEntity.inventory.getStackInSlot(8);
+//	    if (!Md2SSDII.isEmpty()) {
+//		    pPoseStack.pushPose();
+//		    pPoseStack.translate(0.0D, 0.25D, 0.0D);
+//		    pPoseStack.scale(0.4F, 0.4F, 0.4F);
+//
+//		    itemRenderer.renderStatic(
+//				    Md2SSDII,
+//				    ItemDisplayContext.FIXED,
+//				    pPackedLight,
+//				    pPackedOverlay,
+//				    pPoseStack,
+//				    pBufferSource,
+//				    level,
+//				    0
+//		    );
+//		    pPoseStack.popPose();
+//	    }
 
 		// PSU (slots 11)
 
